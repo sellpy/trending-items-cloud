91171bcc782a6acadd96bb3de3dbf8333b455e5a
Jonas Stendahl <jonas.stendahl@outlook.com>
2022-08-19 10:37:49 +0200

    Basic tag cloud

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { ItemCloud } from './ItemCloud'
 
 const App = () => {
-  return <h1>Welcome to React App thats build using Parcel</h1>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <ItemCloud />
+    </div>
+  )
 }
 
 export default App
