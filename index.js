0e9cb97a595a2cb29a622cf3bbd3e246c8842398
Jonas Stendahl <jonas.stendahl@outlook.com>
2022-08-19 10:29:49 +0200

    Add linting and formatting

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import reactDom from 'react-dom'
 import App from './src/App'
 
-reactDom.render(<App />, document.getElementById('app'));
+reactDom.render(<App />, document.getElementById('app'))
